55e36874dcb28c4b6b354b05085e81b18d08bbcc
jakubbibro <jakub.bibro@servicepartner.one>
2020-02-13 15:08:03 +0100

    init

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,29 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.beans
+import org.springframework.web.reactive.function.server.router
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
+class BeansInitializer : ApplicationContextInitializer<GenericApplicationContext> {
+	override fun initialize(context: GenericApplicationContext) {
+		appBeans().initialize(context)
+	}
+}
+
+fun appBeans() = beans {
+	bean { routes() }
+}
+
+fun routes() = router {
+	GET("/demo") { ok().bodyValue("DEMO") }
+}
