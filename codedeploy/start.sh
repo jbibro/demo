666dd2e0c49a8a317eeb22c9d9079fa7a6186459
jakubbibro <jakub.bibro@servicepartner.one>
2020-02-14 08:42:33 +0100

    extract jar to see what's inside

--- codedeploy/start.sh
+++ codedeploy/start.sh
@@ -1,1 +1,2 @@
-java -Djava.security.egd=file:/dev/./urandom -jar /home/ec2-user/demo-0.0.1-SNAPSHOT.jar
+#java -Djava.security.egd=file:/dev/./urandom -jar demo-0.0.1-SNAPSHOT.jar
+jar -xf demo-0.0.1-SNAPSHOT.jar
