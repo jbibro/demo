2113afc2f9a3bda2e080bd44a1e7f22e9322c75f
jakubbibro <jakub.bibro@servicepartner.one>
2020-02-15 12:30:43 +0100

    trigger build

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -31,7 +31,7 @@ fun appBeans() = beans {
 fun routes() = router {
 	GET("/demo") { ok().body(
 			Mono.fromCallable { InetAddress.getLocalHost().hostName }
-					.map { "TEST4 - $it" }
+					.map { "TEST5 - $it" }
 					.subscribeOn(Schedulers.elastic())
 	)
 	}
